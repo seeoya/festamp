d9454db770de7b5287be51f01971c4200a65dfeb
seeoya <seeoya@naver.com>
2023-12-26 14:43:54 +0900

    Wrap 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Wrap from "./component/Wrap";
 
 function App() {
     return (
         <div className="App">
             APP
+            <Wrap />
         </div>
     );
 }
