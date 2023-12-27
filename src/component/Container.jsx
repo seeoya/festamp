400f1d2b256ba1450133dead7ef6032d5b437bf5
seeoya <seeoya@naver.com>
2023-12-27 15:44:19 +0900

    main 컴포넌트에 데이터 연결

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -3,13 +3,15 @@ import Main from "./main/Main";
 
 const Container = (props) => {
     useEffect(() => {
+        // 데이터 가져오는 법
         console.log(props.data);
+        console.log(props.data[0].title);
     }, []);
+
     return (
         <div id="container">
-            {props.data[0].title}
             {/* 여기에 본문 컴포넌트 삽입 */}
-            <Main />
+            <Main data={props.data} />
         </div>
     );
 };
