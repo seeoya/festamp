bd4008f89ab0939f549f6c4a18098f4acbec5cb9
choiheebeom <appel4192@gmail.com>
2023-12-27 18:29:53 +0900

    ListView , CSS 적용

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Main from "./main/Main";
+import ListView from "./list/ListView";
 
 const Container = () => {
     return (
         <div id="container">
             {/* 여기에 본문 컴포넌트 삽입 */}
             <Main />
+            <ListView />
         </div>
     );
 };
