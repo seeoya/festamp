ee1f051adcc86dd737f3d55057c4d85188faf79e
seeoya <seeoya@naver.com>
2023-12-27 14:35:00 +0900

    map 임시 추가

--- src/component/map/Map.jsx
+++ src/component/map/Map.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
+const { kakao } = window;
 
 const Map = () => {
+    useEffect(() => {
+        let container = document.getElementById("map");
+        let options = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3,
+        };
+
+        let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    }, []);
+
     return (
         <div>
-            {/* 맵 가져오기 */}
-            {/* list ver / view ver */}
-            {/* 좌표 불러오는 기능 */}
+            <div
+                id="map"
+                style={{
+                    width: "400px",
+                    height: "500px",
+                }}
+            ></div>
         </div>
     );
 };
