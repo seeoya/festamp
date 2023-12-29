089ee8d9c40e72188207a39181c136a52f9668fe
choiheebeom <appel4192@gmail.com>
2023-12-29 14:23:35 +0900

    주석 , css , listview 약간 수정

--- src/component/list/ListView.jsx
+++ src/component/list/ListView.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './listStyle.css';
 
-const ListView = (props) => {
+const ListView = (festivalData) => {
     
     let calendar = <i class="fa-regular fa-calendar-days"></i>
     let place = <i class="fa-solid fa-compass"></i>
@@ -9,37 +9,37 @@ const ListView = (props) => {
     let name = <i class="fa-solid fa-building"></i>
     let tel = <i class="fa-solid fa-phone-volume"></i>
 
-
     return (
+
+        
 
         <div id="listview_wrap">
             <div className="title">
-                <h1>축제 이름</h1>
+                <h1>{festivalData.festivalData[1].title}</h1>
             </div>
 
             <div className="date">
-                <h1>축제 기간</h1>
+                <h1>{festivalData.festivalData[1].date}</h1>
             </div>
 
             <div className="img">
-                <img src="imgs/dummy/img1.jpg"/>
+                <img src={`/${festivalData.festivalData[0].img}`}/>
             </div>
-
             <div className="explan">
-                <h1>축제 설명</h1>
+                <h1>{festivalData.festivalData[1].explain}</h1>
             </div>
             <div className="middle">
                 <div className="event">
-                    <h1>행사 내용</h1>
+                    <h1>{festivalData.festivalData[1].event}</h1>
                 </div>
 
                 <div className="total">
                     <ul>
-                        <li>{calendar} &nbsp;축제 기간 : </li>
-                        <li>{place} &nbsp;위치 : </li>
-                        <li>{price} &nbsp;가격 : </li>
-                        <li>{name} &nbsp;상호명 : </li>
-                        <li>{tel} &nbsp;전화번호 : </li>
+                        <li>{calendar} &nbsp;{festivalData.festivalData[1].date} </li>
+                        <li>{place} &nbsp;{festivalData.festivalData[1].location} </li>
+                        <li>{price} &nbsp;{festivalData.festivalData[1].price} </li>
+                        <li>{name} &nbsp;{festivalData.festivalData[1].store} </li>
+                        <li>{tel} &nbsp;{festivalData.festivalData[1].tel} : </li>
                     </ul>
                 </div>
             </div>
