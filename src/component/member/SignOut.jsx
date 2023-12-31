bd9cf5d6f425bea67bc499e15083234b4b6e33c1
SUNA <snsong91@gmail.com>
2023-12-31 14:07:15 +0900

    로그아웃버튼 생성 및 네비게이터설정

--- src/component/member/SignOut.jsx
+++ src/component/member/SignOut.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function SignOut() {
     console.log('SignOut() Called!');
+    let navigate = useNavigate();
+
+    const SignOutBntHandler = () => {
+      console.log('SignOutBntHandler() Clicked!');
+
+      navigate("/Main");
+    }
+
+    const NoSignOutBntHandler = () => {
+      console.log('NoSignOutBntHandler() Clicked!');
+
+      navigate("/Main");
+    }
 
   return (
-    <div>SignOut</div>
+    <div>
+      로그아웃 하시겠습니까? <br />
+      <button  onClick={SignOutBntHandler} >&nbsp; 예 &nbsp;</button> &nbsp;
+      <button onClick={NoSignOutBntHandler}>아니오</button>
+
+
+    </div>
   );
 }
 
