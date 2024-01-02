b41536f0973f43fb88052db78bde6322e43b5d33
SUNA <snsong91@gmail.com>
2024-01-03 02:54:09 +0900

    비밀번호 찾기 버튼 함수 생성

--- src/component/member/PwFind.jsx
+++ src/component/member/PwFind.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ChangePw from './ChangePw';
 
 const PwFind = () => {
 
     console.log('IdFind() !!!!!!!!!');
 
     const [uId, setUId] = useState('');
-    const [uEmail, setUEmail] = useState('');
+    const [uPhone, setUphone] = useState('');
+
+    // change password 화면  
+    const[isUInfo, setIsUInfo] = useState(false);
 
     // 네비게이트
     const navigate = useNavigate();
@@ -14,33 +18,49 @@ const PwFind = () => {
     // 로그인뷰 
     const [isLoginView, setIsLoginView] = useState(false);
 
+    // input 입력값
     const inputIdHandler = (e) => {
-        console.log('inputEmailHandler() input');
+        console.log('inputphoneHandler() input');
 
         setUId(e.target.value);
     }
 
-    const inputEmailHandler = (e) => {
+    const inputphoneHandler = (e) => {
         console.log('inputPasswordHandler() input');
 
-        setUEmail(e.target.value);
+        setUphone(e.target.value);
     }
 
+    
+
     // 비밀번호 찾기 버튼 클릭시 
-    const findPwHandler = () => {
-        console.log('findIdHandler() Click!!!!!!');
+    const findPwBtnHandler = () => {
+        console.log('findPwBtnHandler() Clicked !!');
 
         let memberInStorage = JSON.parse(localStorage.getItem('memberDB'));
         let memIdObjarr = Object.keys(memberInStorage);
-        console.log(memberInStorage);
+        console.log(memberInStorage[uId].phone);
         console.log(memIdObjarr);
-        
-        
-        // console.log(memberInStorage[uId].email);
-        // console.log(memberInStorage[uId].pw);
+        console.log(uPhone);
 
+
+        if(uId !== '' && uPhone !== '' ){
+            if(uId !== null  && uId !== undefined ){
+                if(uPhone !== null && uPhone !== undefined ){
+                    
+                setIsUInfo(true);
+                navigate("/ChangePw");
+            }
+            } else{
+                alert('존재하지 않는 ID입니다.');
+            } 
+        } else {
+            alert('정보를 입력해주세요.');
+        }
+        
         
     }
+    // 로그인화면 버튼 클릭시 이동
     const signInView = () => {
         console.log('signInView() Clicked!!!!');
 
@@ -58,14 +78,20 @@ const PwFind = () => {
                     <input type="text" id='u_id' value={uId} onChange={(e) => inputIdHandler(e)} placeholder='ID를 입력하세요' />
                 
 
-                <label htmlFor="u_email">
+                <label htmlFor="u_phone">
                     <p>Phone Number</p>  </label> 
-                    <input type="text" id='u_email' value={uEmail} onChange={(e) => inputEmailHandler(e)} placeholder='Phone Number를 입력하세요' />
+                    <input type="text" id='u_phone' value={uPhone} onChange={(e) => inputphoneHandler(e)} placeholder='Phone Number를 입력하세요' />
                <br />
 
-                <button onClick={findPwHandler}>비밀번호 찾기</button> &nbsp; 
+                <button onClick={findPwBtnHandler}>비밀번호 변경</button> &nbsp; 
                 <button value={isLoginView} onClick={signInView}>로그인 화면</button>
 
+                {isUInfo
+                ?
+                <ChangePw />
+                :
+                null
+                }
             </div>
     </div>
   )
