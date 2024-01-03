59f192ea95359a46aea3ebd188a5bb5b92862797
post50 <ppm4ppm4@gmail.com>
2024-01-03 11:14:15 +0900

    별점 input 버튼 삭제

--- src/component/grade_select/GradeSelect.jsx
+++ src/component/grade_select/GradeSelect.jsx
@@ -4,7 +4,6 @@ import "./gradeSelect.css";
 const GradeSelect = (props) => {
     // hook
     const [inStar, setInStar] = useState("");
-    const [isRewrite, setIsRewrite] = useState(false);
 
     // 리뷰 데이터 목록에 점수 넣기
     useEffect(() => {
@@ -54,25 +53,9 @@ const GradeSelect = (props) => {
         props.setStar(inStar);
     };
 
-    // const inputStar = (e) => {
-    //     console.log("inputStar() CALLED");
-
-    //     
-    //     setInStar(e.target.value);
-    //     console.log(inStar);
-
-<<<<<<< HEAD
-
-    // }
-=======
-        props.setStar(inStar);
-    };
->>>>>>> 1ba682053232f363fda8cdd4ef15dd684674873d
-
     return (
         <>
             <div className="star_wrap">
-<<<<<<< HEAD
 
                 <div className="star_rating space-x-4 mx-auto">
                     <input
@@ -132,73 +115,6 @@ const GradeSelect = (props) => {
                 &nbsp;&nbsp;&nbsp;
                 <div className="star_rate">
                     <span>{inStar}</span>                
-=======
-                {isRewrite ? null : (
-                    <div className="star_rating space-x-4 mx-auto">
-                        <input
-                            onClick={(e) => starClickHandler(e)}
-                            type="radio"
-                            id="5_stars"
-                            name="rating"
-                            value={5}
-                        />
-                        <label for="5_stars" className="star pr-4">
-                            ★
-                        </label>
-                        <input
-                            onClick={(e) => starClickHandler(e)}
-                            type="radio"
-                            id="4_stars"
-                            name="rating"
-                            value={4}
-                        />
-                        <label for="4_stars" className="star">
-                            ★
-                        </label>
-                        <input
-                            onClick={(e) => starClickHandler(e)}
-                            type="radio"
-                            id="3_stars"
-                            name="rating"
-                            value={3}
-                        />
-                        <label for="3_stars" className="star">
-                            ★
-                        </label>
-                        <input
-                            onClick={(e) => starClickHandler(e)}
-                            type="radio"
-                            id="2_stars"
-                            name="rating"
-                            value={2}
-                        />
-                        <label for="2_stars" className="star">
-                            ★
-                        </label>
-                        <input
-                            onClick={(e) => starClickHandler(e)}
-                            type="radio"
-                            id="1_star"
-                            name="rating"
-                            value={1}
-                        />
-                        <label for="1_star" className="star">
-                            ★
-                        </label>
-                    </div>
-                )}
-                &nbsp;&nbsp;&nbsp;
-                <div className="star_rate">
-                    <span>{inStar}</span>
-                    <button onClick={inputStar} />
-                </div>
-                <div className="selected_star">
-                    {isRewrite ? (
-                        <>
-                            ★ <span>{props.star}</span>
-                        </>
-                    ) : null}
->>>>>>> 1ba682053232f363fda8cdd4ef15dd684674873d
                 </div>
                 
             </div>
