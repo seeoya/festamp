fabd2cf599b87aa0dc252f9ef8742e545096a112
choiheebeom <appel4192@gmail.com>
2024-01-03 14:30:57 +0900

    지도 추가 , css 손 보기

--- src/component/view/View.jsx
+++ src/component/view/View.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import MainReview from "../review/MainReview";
+import Map from "../map/Map";
 import "./viewStyle.css";
+import { festivalData } from "../../data/festivalData";
 
 const ListView = (props) => {
     let calendar = <i className="fa-regular fa-calendar-days"></i>;
@@ -9,8 +11,12 @@ const ListView = (props) => {
     let price = <i className="fa-solid fa-copyright"></i>;
     let name = <i className="fa-solid fa-building"></i>;
     let tel = <i className="fa-solid fa-phone-volume"></i>;
-
+    let festData = props.festivalData
+    let { id } = useParams();
+    console.log(id);
     const [eventsArr, setEventsArr] = useState([]);
+    const [nowAddress, setNowAddress] = useState(festData[id].location);
+    const [nowAddressTitle, setNowAddressTitle] = useState(festData[id].title)
 
     useEffect(() => {
         console.log("[ListView] useEffect!!");
@@ -20,57 +26,64 @@ const ListView = (props) => {
     }, []);
 
     // useParams > path="/view/:id" id 값을 가져옴 url 파라미터
-    let { id } = useParams();
-    console.log(id);
 
     return (
         <div id="listview_wrap" className="sec">
             <div className="title">
-                <p>{props.festivalData[id].title}</p>
+                <p className="sec_item_title">{festData[id].title}</p>
             </div>
 
             <div className="date">
-                <h1>{props.festivalData[id].date}</h1>
+                <h1>{festData[id].date}</h1>
             </div>
             <div className="flex_wrap">
                 <div className="middle">
                     <div className="img">
-                        <img src={`/${props.festivalData[id].img}`} />
+                        <img src={`/${festData[id].img}`} />
                     </div>
                     {/* <div className="event">
-                            <h1>{props.festivalData[id].event}</h1>
+                            <h1>{festData[id].event}</h1>
                     </div> */}
                     <div className="explan">
-                        <p>{props.festivalData[id].explain}</p>
+                        <p>{festData[id].explain}</p>
                     </div>
                 </div>
 
                 <div className="bottom">
-                    <ul className="event">
+                    <ul className="event ">
                         {eventsArr.map((event, idx) => {
                             return (
-                                <li className="event_list" key={idx}>
+                                <li className="event_list sec_item" key={idx}>
                                     {event}
                                 </li>
                             );
                         })}
                     </ul>
-                    <div className="info">
+                    <div className="info sec_item">
                         <ul>
                             <li>
-                                {calendar} &nbsp;{props.festivalData[id].date}{" "}
+                                {calendar} &nbsp;{festData[id].date}{" "}
                             </li>
                             <li>
-                                {place} &nbsp;{props.festivalData[id].location}{" "}
+                                {place} &nbsp;{festData[id].location}{" "}
+                                <Map
+                                    festivalData={props.festivalData}
+                                    width={"100%"}
+                                    height={"100%"}
+                                    nowAddress={nowAddress}
+                                    setNowAddress={setNowAddress}
+                                    nowAddressTitle={nowAddressTitle}
+                                    setNowAddressTitle={setNowAddressTitle}
+                                />
                             </li>
                             <li>
-                                {price} &nbsp;{props.festivalData[id].price}{" "}
+                                {price} &nbsp;{festData[id].price}{" "}
                             </li>
                             <li>
-                                {name} &nbsp;{props.festivalData[id].store}{" "}
+                                {name} &nbsp;{festData[id].store}{" "}
                             </li>
                             <li>
-                                {tel} &nbsp;{props.festivalData[id].tel}{" "}
+                                {tel} &nbsp;{festData[id].tel}{" "}
                             </li>
                         </ul>
                     </div>
@@ -79,8 +92,8 @@ const ListView = (props) => {
             <div className="review">
                 <>
                     <MainReview
-                        festivalDataId={props.festivalData[id].id}
-                        festivalTitle={props.festivalData[id].title}
+                        festivalDataId={festData[id].id}
+                        festivalTitle={festData[id].title}
                         loginInfo={props.loginInfo}
                     />
                 </>
