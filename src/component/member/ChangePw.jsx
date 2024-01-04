1163bb6436729ef953356ab388fcd2c7bb7b4e45
SUNA <snsong91@gmail.com>
2024-01-05 06:41:19 +0900

    문구수정

--- src/component/member/ChangePw.jsx
+++ src/component/member/ChangePw.jsx
@@ -69,7 +69,7 @@ const ChangePw = (props) => {
                 alert('비밀번호가 일치하지 않습니다.')
             }
         } else {
-            alert('새 비밀번호를 입력해주세요.');
+            alert('정확한 정보를 입력해주세요.');
         }
     } else {
         alert('존재하지 않는 회원입니다.');
