50020ab982d7c5faef77048d2f1170e7be459125
suna <snsong91@gmail.com>
2024-01-05 11:08:12 +0900

    삼항연산자 수정

--- src/component/member/Privacy.jsx
+++ src/component/member/Privacy.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './SignUp.css';
 import Secession from "./Secession";
+import SignIn from "./SignIn";
 
 const Privacy = (props) => {
     console.log("Privacy() Called!!");
@@ -141,74 +142,75 @@ const Privacy = (props) => {
 
     return (
         <>
-            {
-                isLogIned ?
-                    <div id="sign_up" className="sec member" > 
-                        <div className="sec_item">
-                            <h1 className="sec_item_title">개인정보수정</h1>
-
-                            <div className="sec_item_content">
-                              <div>
-                                <label htmlFor="u_name">이름(닉네임) </label>
-                                <input type="text" id="u_name" name="u_name" className="input"  defaultValue={memberInfo[logInId].name}
-                                    onChange={nickChageHandler} placeholder="이름(닉네임)" />
-                              </div>
-                              <div>
-                                <label htmlFor="u_id">아이디</label>
-                                <input type="text" readOnly id="u_id" name="u_id" className="input"  defaultValue={logInId} />
-                              </div>
-                              <div>
-                                <label htmlFor="u_pw">
-                                    비밀번호
-                                </label>
-                                <div className="btn_wrap" >
-                                  <input type="password" id="u_pw" name="u_pw" className="input"  defaultValue={memberInfo[logInId].pw}
-                                      onChange={pwChangeHadler} placeholder="비밀번호 " />
-                                  
-                                </div>
-                                <label htmlFor="u_pw" className="desc">비밀번호는 영문 대소문자, 숫자, 특수문자를 혼합하여 8~20자로 입력해주세요</label>
-                              </div>
-                              <div>
-                                <label htmlFor="pw_same">비밀번호 확인</label>
-                                <div className="btn_wrap">
-                                  <input
-                                      type="password"
-                                      id="pw_same"
-                                      name="pw_same" className="input"  defaultValue={memberInfo[logInId].pw}
-                                      onChange={pwSameChangeHadler} placeholder="비밀번호 확인"
-                                  />
-                                 
-                                </div>
-                              </div>
-                              <div>
-                                <label htmlFor="u_phone">연락처</label>
-                                <input type="text" id="u_phone" name="u_phone" className="input"  defaultValue={memberInfo[logInId].phone}
-                                    onChange={phoneChangeHandler} placeholder="연락처" />
-                              </div>
-                              <div>
-                                <label htmlFor="u_email">이메일</label>
-                                <input type="email" id="u_email" name="u_email" className="input"  defaultValue={memberInfo[logInId].email}
-                                    onChange={eMailChangeHandler} placeholder="E-mail" />
-                              </div>
-                              <div>
-                                <label htmlFor="u_birth">생년월일</label>
-                                <input type="date" id="u_birth" name="u_birth" className="input"  defaultValue={memberInfo[logInId].birth}
-                                    onChange={birthChangeHandler} placeholder="생일" />
-                              </div>
-                              <button type="button" className="btn"  onClick={changeBtnHandler}>정보수정</button>
-                              <button type="button" className="btn"  onClick={ secessinoViewHandler}>회원탈퇴</button>
-
-                                                      </div>
-                            </div>
-                    </div >
-                    : null}
-                    {
+            {isLogIned?
+            
+                    
                         isSecession
                         ?
                         <Secession loginInfo={props.loginInfo} />
                         :
-                        null
-                    }
+                        
+                    <div id="sign_up" className="sec member" > 
+                    <div className="sec_item">
+                        <h1 className="sec_item_title">개인정보수정</h1>
+
+                        <div className="sec_item_content">
+                          <div>
+                            <label htmlFor="u_name">이름(닉네임) </label>
+                            <input type="text" id="u_name" name="u_name" className="input"  defaultValue={memberInfo[logInId].name}
+                                onChange={nickChageHandler} placeholder="이름(닉네임)" />
+                          </div>
+                          <div>
+                            <label htmlFor="u_id">아이디</label>
+                            <input type="text" readOnly id="u_id" name="u_id" className="input"  defaultValue={logInId} />
+                          </div>
+                          <div>
+                            <label htmlFor="u_pw">
+                                비밀번호
+                            </label>
+                            <div className="btn_wrap" >
+                              <input type="password" id="u_pw" name="u_pw" className="input"  defaultValue={memberInfo[logInId].pw}
+                                  onChange={pwChangeHadler} placeholder="비밀번호 " />
+                              
+                            </div>
+                            <label htmlFor="u_pw" className="desc">비밀번호는 영문 대소문자, 숫자, 특수문자를 혼합하여 8~20자로 입력해주세요</label>
+                          </div>
+                          <div>
+                            <label htmlFor="pw_same">비밀번호 확인</label>
+                            <div className="btn_wrap">
+                              <input
+                                  type="password"
+                                  id="pw_same"
+                                  name="pw_same" className="input"  defaultValue={memberInfo[logInId].pw}
+                                  onChange={pwSameChangeHadler} placeholder="비밀번호 확인"
+                              />
+                             
+                            </div>
+                          </div>
+                          <div>
+                            <label htmlFor="u_phone">연락처</label>
+                            <input type="text" id="u_phone" name="u_phone" className="input"  defaultValue={memberInfo[logInId].phone}
+                                onChange={phoneChangeHandler} placeholder="연락처" />
+                          </div>
+                          <div>
+                            <label htmlFor="u_email">이메일</label>
+                            <input type="email" id="u_email" name="u_email" className="input"  defaultValue={memberInfo[logInId].email}
+                                onChange={eMailChangeHandler} placeholder="E-mail" />
+                          </div>
+                          <div>
+                            <label htmlFor="u_birth">생년월일</label>
+                            <input type="date" id="u_birth" name="u_birth" className="input"  defaultValue={memberInfo[logInId].birth}
+                                onChange={birthChangeHandler} placeholder="생일" />
+                          </div>
+                          <button type="button" className="btn"  onClick={changeBtnHandler}>정보수정</button>
+                          <button type="button" className="btn"  onClick={ secessinoViewHandler}>회원탈퇴</button>
+
+                                                  </div>
+                        </div>
+                </div >
+                
+                    
+                    :null}
         </>
     );
 };
