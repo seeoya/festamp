4c451a8ff85d07b93dc93130f67db62421942147
seeoya <seeoya@naver.com>
2024-01-05 09:51:03 +0900

    not found 추가

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -21,6 +21,7 @@ const Header = (props) => {
                         RANKING
                     </Link>
                 </li>
+                
                 {isLogIned ? (
                     <>
                         <li>
