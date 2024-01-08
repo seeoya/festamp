d7cd100523dfdb1a38baff6ee4f181fd376f4ab3
seeoya <seeoya@naver.com>
2024-01-08 15:34:43 +0900

    build 저장

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import Wrap from "./component/Wrap";
-
-function App() {
-    return <Wrap />;
-}
-
-export default App;
--- src/component/util/Random.js
+++
@@ -1,9 +0,0 @@
-const customRandom = (startNum, endNum) => {
-    // startNum 이상 ~ endNum 이하 랜덤 정수 return
-    let result = Math.floor(Math.random() * (endNum - 1) + startNum);
-
-    return result;
-};
-
-export { customRandom };
-
